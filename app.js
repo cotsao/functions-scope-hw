a0ae3e3334f77ff1d44345fb7df4292f35c630cd
cotsao <codytsao@gmail.com>
2021-07-09 11:35:17 -0700

    Commit 1 - Verbal questions

---
+++ app.js
@@ -0,0 +1,13 @@
+/* Cody Tsao
+SEI 7.06
+Functions Deliverable */
+
+// 1. Verbal questions
+/* 
+1. What is the difference between a parameter and an argument?
+Parameters are the variables declared when creating the function. Arguments are the variables you pass into the function when calling it.
+2. Within a function, what is the difference between return and console.log?
+Return allows a function to output a value, console.log just prints to the console. i.e. Return statements return something tangible that can be used, console.log just prints something.
+3. What are the implications of the ability of a function to return a value? 
+This allows the programmer to more concisely get desired values. For instance, if a function was created to get the min number of an array, the return function would allow the programmer to use that number more easily.
+*/
